243b4f30a1ca92c88b37abbed700c046c4036d53
Mitali Rawat <mitalirawat24@gmail.com>
2018-09-26 14:56:32 -0500

    Fixing main.py

--- fall18/p3/main.py
+++ fall18/p3/main.py
@@ -1,4 +1,4 @@
-import project 
+import project
 
 # my-login: YOUR-NETID
 # partner-login: PARTNER-NETID
@@ -22,11 +22,11 @@ def testGetNumRecords():
 def serachHurricane(name):
     #create counter here
     while(False): #stopping condition, this will currently not loop, fix it
-        #code you want to occur every loop goes here     
+        #code you want to occur every loop goes here
         pass
         #code you want to occur every loop stops here
         #increment should go here
-    return 0 
+    return 0
 
 #Function 5
 #searchHurricaneByLocation(lat, long)
@@ -34,13 +34,13 @@ def serachHurricane(name):
 
 
 #Function 6
-#countHurricane(oceanName)       
-        
+#countHurricane(oceanName)
+
 
 #Function 7
 #maxWindSpeedHurricane()
 
-#Function 8 
+#Function 8
 #minWindSpeedHurricane()
 
 def main():
@@ -48,42 +48,42 @@ def main():
     print("The number of records in the DataSet is ")
     print(0)
     print()
-    
+
     print("Problem 2")
     print("The name of the hurricane in the 20th row of the dataset is ")
     print(0)
     print()
-    
+
     print("Problem 3")
     print("Does the hurricane 'BOB' Exist in the Dataset?")
-    print(True)
+    print(0)
     print()
-    
+
     print("Problem 4")
     print("Does the hurricane 'BRITNEY' Exist in the Dataset?")
     print(0)
     print()
-    
+
     print("Problem 5")
     print("Find the hurricane that came at 9.0N Latitude and 157.0W Longitude")
     print(0)
     print()
-    
+
     print("Problem 6")
     print("How many hurricanes came from the Pacific?")
     print(0)
     print()
-    
+
     print("Problem 7")
     print("How many hurricanes came from the Atlantic?")
     print(0)
     print()
-    
+
     print("Problem 8")
     print("What is the difference between the max Windspeed and min Windspeed of the dataSet?")
     print(0)
     print()
 if __name__ == '__main__':
-    main()    
+    main()
 
 
